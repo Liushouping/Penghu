cf8a8d105cc1ce55cfce22d2313acdf6208daf4d
liushouping <kose146352@gmail.com>
2024-04-16 16:03:16 +0800

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -18,14 +18,141 @@
       </div>
       <!-- Card -->
       <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-        <div class="bg-gray-300 w-full h-[300px]">01</div>
-        <div class="bg-gray-300 w-full h-[300px]">02</div>
-        <div class="bg-gray-300 w-full h-[300px]">03</div>
-        <div class="bg-gray-300 w-full h-[300px]">04</div>
-        <div class="bg-gray-300 w-full h-[300px]">05</div>
-        <div class="bg-gray-300 w-full h-[300px]">06</div>
-        <div class="bg-gray-300 w-full h-[300px]">07</div>
-        <div class="bg-gray-300 w-full h-[300px]">08</div>
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
       </div>
     </div>
     <!-- 澎湖交通辦法 -->
@@ -42,12 +169,102 @@
       </div>
       <!-- Card -->
       <div class="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-        <div class="bg-gray-300 w-full h-[300px]">01</div>
-        <div class="bg-gray-300 w-full h-[300px]">02</div>
-        <div class="bg-gray-300 w-full h-[300px]">03</div>
-        <div class="bg-gray-300 w-full h-[300px]">04</div>
-        <div class="bg-gray-300 w-full h-[300px]">05</div>
-        <div class="bg-gray-300 w-full h-[300px]">06</div>
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
+        <div class="w-full rounded overflow-hidden shadow-lg tracking-[0px]">
+          <div class="w-full h-300 relative">
+            <img src="https://via.placeholder.com/300" alt="Card image" class="w-full h-full object-cover">
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <div class="font-bold text-xl mb-2">暑假歐亞非｜折1萬</div>
+            <p class="text-gray-700 text-[12px]">
+              歐洲品酒、頭等列車、當地纜車 特選印度、埃及、印度秘境遊
+            </p>
+          </div>
+          <div class="px-2 py-4 bg-white">
+            <p class="text-red-500 text-[12px] text-right">
+              $ 900
+            </p>
+          </div>
+        </div>
       </div>
     </div>
     <!-- 澎湖美食饗宴 -->
