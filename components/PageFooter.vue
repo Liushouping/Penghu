f564861e8ed36e541721d97492d314bde9c5455a
liushouping <kose146352@gmail.com>
2024-04-16 14:39:02 +0800

    test

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -3,7 +3,7 @@ const Year = new Date().getFullYear();
 </script>
 <template>
     <div class="w-full h-full flex justify-center items-center bg-yellow-300">
-        <div class="px-4 sm:px-20 py-20 flex justify-between items-center">
+        <div class="max-w-[1200px] w-full px-4 xl:px-0 py-20 flex justify-between items-center">
             <ul>
                 <li>海迷禮</li>
                 <li>1.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
