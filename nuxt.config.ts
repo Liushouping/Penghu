f7c2829c148d4aa0da7dffe83c90f9e488022d17
liushouping <kose146352@gmail.com>
2024-04-18 10:49:04 +0800

    Test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,11 @@ export default defineNuxtConfig({
       },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: '航海王動畫25週年璀璨之夜 ',
+      title: '航海王動畫25週年璀璨之夜 X 2024澎湖國際海上花火節',
       meta: [
-        { name: 'description', content: '2024年你不能錯過的盛事，草帽一行人霸氣登場！' },
-        { property: 'og:title', content: '航海王動畫25週年璀璨之夜 ' },
-        { property: 'og:description', content: '2024年你不能錯過的盛事，草帽一行人霸氣登場！' },
+        { name: 'description', content: '2024年你不能錯過的精彩盛事，草帽一行人霸氣登場！ 首次以航海王為主題的無人機及煙火表演，讓人回味草帽一行人冒險感動的星光電影院，氣勢滂薄交響樂及熱血沸騰的歌曲表演。一系列週邊限定商品、主題輕食等，帶給你滿滿的驚喜，一起與草帽一行人前往澎湖冒險！與魯夫齊聲呼喊一起成為海賊王吧！' },
+        { property: 'og:title', content: '航海王動畫25週年璀璨之夜 X 2024澎湖國際海上花火節' },
+        { property: 'og:description', content: '2024年你不能錯過的精彩盛事，草帽一行人霸氣登場！ 首次以航海王為主題的無人機及煙火表演，讓人回味草帽一行人冒險感動的星光電影院，氣勢滂薄交響樂及熱血沸騰的歌曲表演。一系列週邊限定商品、主題輕食等，帶給你滿滿的驚喜，一起與草帽一行人前往澎湖冒險！與魯夫齊聲呼喊一起成為海賊王吧！' },
         { property: 'og:image', content: '/' }
       ],
       link: [
