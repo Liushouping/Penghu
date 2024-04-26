bece242ef177db440dfcf8885bc25c3d3aa3741b
liushouping <kose146352@gmail.com>
2024-04-26 18:03:13 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -479,11 +479,11 @@ const alert = ref("true");
 
         <div class="relative bg-gray-50 rounded-md flex flex-col space-y-2 justify-center items-start pt-8 pb-4 pl-4 pr-4 z-50">
             <div class="absolute right-2 top-2 cursor-pointer z-50" @click="alert = false">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
                     <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
                 </svg>
             </div>
-            <p class="text-[12px] tracking-[2px]">網站限制了圖片的複製功能。</p>
+            <p class="txt-Bold text-[12px] tracking-[2px]">網站限制了圖片的複製功能。</p>
             <p class="text-[12px] tracking-[2px]">The website restricts the copying function of images.</p>
         </div>
       </div>
